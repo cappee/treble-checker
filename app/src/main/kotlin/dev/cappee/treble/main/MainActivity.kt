127a6fa32e0d6358a937f36e055860f54d68a835
Gabriele Cappellaro <gabr.cappellaro@gmail.com>
2021-01-30 18:25:21 +0100

    ✨  Added menu as bottom sheet

--- app/src/main/kotlin/dev/cappee/treble/main/MainActivity.kt
+++ app/src/main/kotlin/dev/cappee/treble/main/MainActivity.kt
@@ -2,11 +2,19 @@ package dev.cappee.treble.main
 
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.afollestad.materialdialogs.LayoutMode
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.afollestad.materialdialogs.list.ItemListener
+import com.afollestad.materialdialogs.list.listItems
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import dev.cappee.treble.R
 import dev.cappee.treble.main.viewpager.ViewPagerAdapter
 import dev.cappee.treble.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
@@ -28,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         override fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {}
         override fun onDrawFrame(gl: GL10) {}
     }
+    private lateinit var menuDialog: MaterialDialog
 
     private lateinit var binding: ActivityMainBinding
 
@@ -36,7 +45,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.title = "Elleueleu"
 
         //Running GPU stuff
         lifecycleScope.launch(Dispatchers.Main) {
@@ -58,6 +66,46 @@ class MainActivity : AppCompatActivity() {
             }
         }.attach()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        menuDialog = MaterialDialog(this, BottomSheet(LayoutMode.WRAP_CONTENT))
+        menuDialog.apply {
+            title(R.string.menu)
+            listItems(null,
+                listOf(getText(R.string.tools), getText(R.string.about_us), getText(R.string.settings)),
+                null,
+                false,
+                object : ItemListener {
+                override fun invoke(dialog: MaterialDialog, index: Int, text: CharSequence) {
+                    when (index) {
+                        0 -> {
+                            //TODO: Open tools activity
+                            dismiss()
+                        }
+                        1 -> {
+                            //TODO: Open about us activity
+                            dismiss()
+                        }
+                        2 -> {
+                            //TODO: Open settings activity
+                            dismiss()
+                        }
+                    }
+                }
+            })
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu -> {
+                menuDialog.show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onResume() {
