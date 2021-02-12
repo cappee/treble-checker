64877674fba456e12841d202af3706e81d3bee5b
Gabriele Cappellaro <gabr.cappellaro@gmail.com>
2021-02-12 19:48:14 +0100

    :pencil2: Updated gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
-    implementation("com.google.android.material:material:1.2.1")
+    implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.core:core-ktx:1.3.2")
